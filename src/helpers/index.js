9b032e471124a8d56f9a092447a3398a1be9db98
sank2000 <santhoshvelr@yahoo.com>
2021-07-18 22:55:47 +0530

    helpers added ✔

---
+++ src/helpers/index.js
@@ -0,0 +1,18 @@
+import { StatusCodes } from 'http-status-codes';
+
+export const sendSuccess = (res, body) => {
+	return res.status(StatusCodes.OK).json(body);
+};
+
+export const sendFailure = (res, body, code = StatusCodes.BAD_REQUEST) => {
+	return res.status(code).json(body);
+};
+
+export const validateBody = (validator, params = false) => {
+	return (req, res, next) => {
+		const { error } = validator(params ? req.params : req.body);
+		if (error) return sendFailure(res, { error: error.details[0].message });
+
+		next();
+	};
+};
